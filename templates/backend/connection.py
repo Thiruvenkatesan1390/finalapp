a88aab7e7ca735ffe6b2a706cab4fd3296bdec67
Thiruvenkatesan1390 <77884035+Thiruvenkatesan1390@users.noreply.github.com>
2021-04-15 12:00:50 +0530

    Update connection.py

--- templates/backend/connection.py
+++ templates/backend/connection.py
@@ -1,1 +1,24 @@
+import sqlite3
 
+#Connecting to sqlite
+conn = sqlite3.connect('tinku.db')
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+#Retrieving data
+cursor.execute('SELECT * from little_prince')
+
+#Fetching 1st row from the table
+result = cursor.fetchall();
+print(result)
+
+#Fetching 1st row from the table
+result = cursor.fetchall();
+print(result)
+
+#Commit your changes in the database
+conn.commit()
+
+#Closing the connection
+conn.close()
